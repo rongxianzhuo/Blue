0ef23807c4e106413bc1fc153d016b04317486be
容显卓 <1535454167@qq.com>
2023-07-30 23:37:32 +0800

    可以动态调整学习率

--- Script/Runtime/Optimizers/AdamOptimizer.cs
+++ Script/Runtime/Optimizers/AdamOptimizer.cs
@@ -7,17 +7,13 @@ namespace Blue.Optimizers
     public class AdamOptimizer : IOptimizer
     {
 
-        private readonly float _learningRate;
+        public float LearningRate = 0.001f;
+        
         private readonly float _beta1 = 0.9f;
         private readonly float _beta2 = 0.999f;
         private readonly Dictionary<ComputeBuffer, ComputeBuffer> _m = new Dictionary<ComputeBuffer, ComputeBuffer>();
         private readonly Dictionary<ComputeBuffer, ComputeBuffer> _v = new Dictionary<ComputeBuffer, ComputeBuffer>();
         private readonly Dictionary<ComputeBuffer, float> _t = new Dictionary<ComputeBuffer, float>();
-
-        public AdamOptimizer(float learningRate=0.001f)
-        {
-            _learningRate = learningRate;
-        }
         
         public void OnBackwardPropagation(ComputeBuffer param, ComputeBuffer gradient)
         {
@@ -37,7 +33,7 @@ namespace Blue.Optimizers
                 t = 1f;
                 _t[param] = t;
             }
-            AdamOperate.Calculate(t, _beta1, _beta2, _learningRate, gradient, m, v, param);
+            AdamOperate.Calculate(t, _beta1, _beta2, LearningRate, gradient, m, v, param);
         }
 
         public void Destroy()
